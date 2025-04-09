0cdefc5d0a1115e6b2b71bd173970986b70e670c
Gonzalo Crucitta <101659340+GonzaloCrucitta@users.noreply.github.com>
2025-04-09 19:31:10 -0300

    Update and rename beta.js to caso0.js

--- caso0.js
+++ caso0.js
@@ -1,3 +1,9 @@
+/* 
+Escenario 0:  Representar como funciona la barrera que corta el paso vehicular en un cruce.
+Tenemos el tren , una calle que es mano unica y tenemos una barrera que puede estar levantada
+o bajada. Cuando termina de pasar el tren, debería levantarse la barrera
+*/
+
 class Barrera {
     constructor(estado){
         this.estadoBarrera = estado;
@@ -6,10 +12,16 @@ class Barrera {
         return this.estadoBarrera;
     }
     subirBarrera(){
-        this.estadoBarrera = "levantada"
+        if(this.estadoBarrera !== "levantada") {
+            console.log("Subiendo la barrera...");
+            this.estadoBarrera = "levantada";
+        }
     }
     bajarBarrera(){
-        this.estadoBarrera = "baja"
+        if(this.estadoBarrera !== "baja") {
+            console.log("Bajando la barrera...");
+            this.estadoBarrera = "baja";
+        }
     }
 }
 
@@ -28,12 +40,14 @@ class Sensor {
 function main(){
     barrera = new Barrera("levantada");
     sensor = new Sensor(barrera)
+
     console.log("El tren esta por pasar. La barrera esta: " + barrera.getEstado());
-    if(barrera.getEstado()=='levantada'){
-       sensor.activar() ;
-    }
+    sensor.activar() ;
+
     console.log("El tren esta pasando. La barrera esta: " +barrera.getEstado());
+    
     sensor.desactivar()
+    
     console.log("El tren termino de pasar. La barrera esta: "+barrera.getEstado())
 
 }
